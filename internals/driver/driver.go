774419f50354d90685bf3dd34b59e2eb33c80148
ahmed_khaled_1 <ahmedkhaledmohamed9@gmail.com>
2022-09-20 01:52:37 +0200

    update separate mongo from sql

--- internals/driver/driver.go
+++ internals/driver/driver.go
@@ -1,14 +1,10 @@
 package driver
 
 import (
-	"context"
 	"database/sql"
 	_ "github.com/jackc/pgconn"
 	_ "github.com/jackc/pgx/v4"
 	_ "github.com/jackc/pgx/v4/stdlib"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"log"
 	"time"
 )
@@ -20,16 +16,9 @@ const (
 	maxIdleDbConn = 5 // how many connection can remain in the pool but idle
 )
 
-var (
-	ctx    context.Context
-	err    error
-	client *mongo.Client
-)
-
 //DB holds the database connection pool
 type DB struct {
-	Mongo *mongo.Client
-	SQL   *sql.DB
+	SQL *sql.DB
 }
 
 //Conn a reference to the DB type
@@ -78,21 +67,4 @@ func openPostgres(dsn string) (*sql.DB, error) {
 	}
 
 	return db, nil
-}
-
-func ConnectMongo(dsn string) *DB {
-	ctx = context.Background()
-	client, err = mongo.Connect(ctx, options.Client().ApplyURI(dsn))
-	if err != nil {
-		panic(err)
-	}
-
-	err = client.Ping(context.TODO(), readpref.Primary())
-	if err != nil {
-		log.Fatal(err)
-	}
-	Conn.Mongo = client
-	log.Println("Connected to MongoDB")
-	return Conn
-
 }
